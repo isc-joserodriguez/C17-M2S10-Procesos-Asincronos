a32594f5eab057477bee2429b2e05a520e8c3961
José Rodriguez <isc.joserodriguez@gmail.com>
2023-05-15 20:35:40 -0700

    Promesas

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+let pokemonInfo = {};
+axios
+  .get('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(function (response) {
+    pokemonInfo.src = response.data.sprites.front_default;
+    pokemonInfo.textContent = response.data.name;
+    document.getElementById('pokemon').src = pokemonInfo.src;
+    document.getElementById('nombre').textContent = pokemonInfo.textContent;
+    // handle success
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log('Hubo un error, verifica tu código');
+  })
+  .finally(function () {
+    // always executed
+    console.log('Siempre se ejecuta');
+  });
 // Funciones flecha
 /**
  * 
@@ -7,23 +26,19 @@
         console.log(msg);
     };
  */
-const objeto = {
+/* const objeto = {
   suma: (a, b) => {
     return a + b;
   },
 };
 
-console.log(objeto.suma(1, 2));
-
-const saludo = (msg) => {
-  console.log(msg);
-};
+console.log(objeto.suma(1, 2)); */
 
 //saludo('Hola');
 
 // setInterval
 
-const interval = setInterval(() => {
+/* const interval = setInterval(() => {
   console.log('Hola');
   clearInterval(interval);
 }, 5000);
@@ -34,3 +49,52 @@ const timer = setTimeout(() => {
 }, 5000);
 
 clearTimeout(timer);
+ */
+let fechaDato = {};
+const saludo = () => {
+  console.log('Hola', fechaDato.date.nuevo);
+};
+
+const obtenerFecha = (saludo, despedida) => {
+  setTimeout(() => {
+    console.log('EJECUTA FECHA');
+    fechaDato = { date: { nuevo: new Date() } };
+    saludo();
+    despedida();
+  }, 5000);
+};
+
+const despedida = () => {
+  console.log('adiós', fechaDato);
+};
+
+// Asincronísmo de JS
+//Callbacks
+//obtenerFecha(saludo, despedida);
+
+// Promesas
+
+// !Siempre resolve & reject van a ser funciones
+const promesa = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const aleatorio = Math.random();
+    if (aleatorio > 0.5) {
+      resolve(aleatorio);
+    } else {
+      reject(aleatorio);
+    }
+  }, 5000);
+});
+
+promesa
+  .then((num) => {
+    console.log('El numero es mayor a .5', num);
+  })
+  .catch((num) => {
+    console.log('El numero es menor a .5', num);
+  })
+  .finally(() => {
+    console.log('Siempre se ejecuta');
+  });
+
+console.log('Termina script');
